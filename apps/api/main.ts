b5fac88cfe944d96917954fae391cca63467f7f4
hvkalayil <hoseakalayil@gmail.com>
2025-03-15 19:04:15 +0530

    feat: added log for more visibility while running both together

--- apps/api/main.ts
+++ apps/api/main.ts
@@ -1,6 +1,6 @@
 import { Application } from "@oak/oak";
 import apiV1 from "./controller/v1/index.ts";
-import { PORT } from "./config.ts";
+import { HOST, PORT } from "./config.ts";
 import { logger } from "./middleware/logger.ts";
 
 const app = new Application();
@@ -9,7 +9,8 @@ app.use(logger);
 app.use(apiV1.routes());
 
 try {
-  console.info(`Starting server on port ${PORT}`);
+  console.info("\x1b[42m\x1b[37m 🐦‍🔥 API ready \x1b[0m");
+  console.info(`\t\x1b[1mLocal:\x1b[0m ${HOST}:${PORT}`);
   await app.listen({ port: Number(PORT) });
 } catch (error) {
   console.error(`Failed to start server: ${error}`);
