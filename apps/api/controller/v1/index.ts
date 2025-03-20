f97f3044855466d3ce11d68bd5ca20a7ef78d3f9
hvkalayil <hoseakalayil@gmai.com>
2025-03-20 16:24:19 +0530

    feat:openapi setup done

--- apps/api/controller/v1/index.ts
+++ apps/api/controller/v1/index.ts
@@ -1,7 +1,19 @@
 import { Router } from "@oak/oak";
+import { setupSwagger } from "./swagger.ts";
 
 const router = new Router({ prefix: "/v1" });
 
+setupSwagger(router);
+
+/**
+ * @openapi
+ * /health:
+ *   get:
+ *     description: Welcome to swagger-jsdoc!
+ *     responses:
+ *       200:
+ *         description: Returns a mysterious string.
+ */
 router.get("/health", (context) => {
   context.response.body = { health: "OK" };
 });
