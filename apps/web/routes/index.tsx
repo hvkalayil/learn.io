d068a55549421bf73d8aeab404a4da010553fb21
Hosea Varghese Kalayil <hoseakalayil@gmail.com>
2025-03-22 23:34:31 +0530

    feat:api setup done in frontend (#10)
    Co-authored-by: hvkalayil <hoseakalayil@gmai.com>

--- apps/web/routes/index.tsx
+++ apps/web/routes/index.tsx
@@ -1,3 +1,21 @@
-export default function Home() {
-  return <h1>Hello World</h1>;
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { apiFetch } from "../lib/http.ts";
+
+const handler: Handlers = {
+  async GET(_, ctx) {
+    try {
+      const result = await apiFetch("/health", "GET");
+      return ctx.render(result);
+    } catch (error) {
+      console.log(error);
+      return new Response("Failed to call the Almighty API", { status: 500 });
+    }
+  },
+};
+
+export default function Home({ data }: PageProps) {
+  console.log(data);
+  return <h1>Hello World. I am {`${data.health}`}</h1>;
 }
+
+export { handler };
