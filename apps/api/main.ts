9c5ffe2e3ecb1ba2e9a6a7435ccbfd8c82f2f3dd
hvkalayil <hoseakalayil@gmai.com>
2025-03-22 22:18:34 +0530

    feat:root endpoint redirected to v1

--- apps/api/main.ts
+++ apps/api/main.ts
@@ -1,12 +1,19 @@
-import { Application } from "@oak/oak";
+import { Application, Router } from "@oak/oak";
 import apiV1 from "./controller/v1/index.ts";
 import { HOST, PORT } from "./config.ts";
 import { logger } from "./middleware/logger.ts";
 
 const app = new Application();
+const router = new Router();
 
 app.use(logger);
 app.use(apiV1.routes());
+
+router.get("/", (context) => {
+  context.response.redirect("/v1/docs");
+});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 try {
   console.info("\x1b[42m\x1b[37m 🐦‍🔥 API ready \x1b[0m");
