5c7bc6ba4bcf4987fbce5c6f611a214c031bbbe0
Hosea Varghese Kalayil <hoseakalayil@gmail.com>
2025-04-15 11:45:32 +0530

    feat:integrated list API (#31)
    Co-authored-by: hvkalayil <hoseakalayil@gmai.com>

--- apps/web/routes/index.tsx
+++ apps/web/routes/index.tsx
@@ -1,13 +1,23 @@
+import { Handlers, PageProps } from "$fresh/server.ts";
 import { HeaderBar } from "../components/HeaderBar.tsx";
+import { HomePageData } from "../context/homepage/types.ts";
 import { Tabs } from "../islands/Tabs.tsx";
+import { apiFetch } from "../lib/http.ts";
 
-export default function Home() {
+export const handler: Handlers<HomePageData> = {
+  async GET(_, ctx) {
+    const result = await apiFetch<HomePageData>("/home", "GET");
+    return ctx.render(result);
+  },
+};
+
+export default function Home({ data }: PageProps<HomePageData>) {
   return (
     <div>
       <HeaderBar />
 
       <main className="p-4 flex flex-col justify-center items-center gap-2">
-        <Tabs />
+        <Tabs data={data} />
       </main>
     </div>
   );
